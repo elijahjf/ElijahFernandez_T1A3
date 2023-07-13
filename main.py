4c6d9a83275628aa56945f230db279b3f4a15457
Elijah <122788521+elijahjf@users.noreply.github.com>
2023-07-13 19:35:39 +1000

    added ingredients and methods nested lists with ability to search them for keywords

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import json
 
 def main():
-    try:
-        # Initialize recipe list
-        recipe_list = []
-        with open("recipes.json", "r") as infile:
-            recipe_list = json.load(infile)
-    except FileNotFoundError:
-        print("The 'recipes.json' file is not found...")
-        print("Creating a new recipe book...")
-        recipe_list = []
+    
+
+    recipe_list = []
         
     selection = 0
     while selection != 4:
@@ -27,25 +21,43 @@ def main():
             name_recipe = input("Enter the name of the recipe: ")
             name_author = input("Enter the author of the recipe: ")
             name_cuisine = input("Enter the cuisine of the recipe: ")
-            recipe_list.append([name_recipe, name_author, name_cuisine])
+            name_total_time = input("Enter the total time of the recipe in minutes: ")
+            name_servings = input("Enter the servings of the recipe: ")
+
+            # Multi-line input for ingredients
+            print("Enter the ingredients of the recipe (type 'end' on a new line to finish):")
+            name_ingredients = []
+            line = input()
+            while line != "end":
+                name_ingredients.append(line)
+                line = input()
+
+            # Multi-line input for method
+            print("Enter the method of the recipe (type 'end' on a new line to finish):")
+            name_method = []
+            line = input()
+            while line != "end":
+                name_method.append(line)
+                line = input()
+
+            recipe_list.append([name_recipe, name_author, name_cuisine, name_total_time, name_servings, name_ingredients, name_method])
         elif selection == 2:
             print("Searching for a recipe...")
             keyword = input("Search: ")
             for i in recipe_list:
-                if keyword in i:
+                if keyword in i or any(keyword in ingredient for ingredient in i[6]):
                     print(i)
+
         elif selection == 3:
             print("Displaying full recipes...")
             for i in range(len(recipe_list)):
                 print(recipe_list[i])
-        else:         
+        else:
             print("Exiting App...")
     print("\nGoodbye!") 
     
-    # Saving to JSON file
-    with open("recipes.json", "w") as outfile:
-        json.dump(recipe_list, outfile)
-
 # if the name of the file = to the main func then go ahead and execute main func
 if __name__ == "__main__":
     main()
+
+# add amounts (1cup of flour) that can be changed if you change servings
